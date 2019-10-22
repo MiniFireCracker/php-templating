ab101fcbb02c46926e5dafda891c530c957a9ba6
Gonzalez <lodi31@neuf.fr>
2019-10-22 14:07:56 +0200

    it works

--- index.php
+++ index.php
@@ -14,11 +14,33 @@ $loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
 $twig = new Twig_Environment($loader, [
   'cache' => false,
 ]);
+$faker = Faker\Factory::create();
 
 switch ($page){
 
   case 'social_profile':
-    echo $twig->render('social_profile.php');
+    echo $twig->render('social_profile.php', ['data' =>[
+    	 'company_name' => $faker->company,
+    'company_catch_phrase' => $faker->catchPhrase,
+    'company_product'=> $faker->word,
+    'company_materials'=> $faker->word,
+    'company_img'=> $faker->imageUrl($width = 640, $height = 480),
+    'company_url'=> $faker->url,
+    'company_color'=> $faker->safeColorName,
+    'company_price'=> $faker->randomDigit,
+
+    'user_name'=>  $faker->userName,
+    'user_img'=> $faker->imageUrl($width, $height, 'cats', true, 'Faker'),
+    'user_job'=> $faker->jobTitle,
+    'user_mail'=> $faker->email,
+    'user_phone'=> $faker->phoneNumber,
+    'user_adress'=> $faker->streetAddress,
+    'user_postcode'=> $faker->postcode,
+    'user_city'=> $faker->city,
+]]
+
+
+);
     break;
 
   default :
--- views/social_profile.php
+++ views/social_profile.php
@@ -5,47 +5,48 @@
 
 <?php 
 
-require_once '../vendor/autoload.php';
+require_once '/vendor/autoload.php';
 $faker = Faker\Factory::create();
 
 ?>
 
         <main class= main>
             <div>
-            <h1><?=$faker->company?></h1>
-            <h2><?=$faker->catchPhrase?></h2>
+            <h1>{{data.company_name}} </h1>
+            <h2>{{data.company_catch_phrase}}</h2>
             </div>
 
             <div>
-                <h3>At <?=$faker->company?>, we create <?= $faker->word ?> made of <?= $faker->word ?>  </h3>
-                <h3>Find out more on  <?=$faker->url ?></h3>
+                <h3>At {{data.company_name}}, we create {{data.company_product}} made of {{data.company_materials}}  </h3>
+                <h3>Find out more on  {{data.company_url}}</h3>
             </div>
 
             <div class=card>
-                <img src=" <?=$faker->imageUrl($width = 640, $height = 480)?>">
-                <h3><?= $faker->word ?></h3>
-                <p> Material:<?= $faker->word ?> </p>
-                <p> Color: <?= $faker->safeColorName  ?> </p>
-                <h3><?= $faker->randomDigit ?>$only!</h3>
+                <img src=" {{data.company_img}}">
+                <h3>{{data.company_product}}</h3>
+                <p> Material: {{data.company_materials}} </p>
+                <p> Color: {{data.company_color}} </p>
+                <h3>{{data.company_price}}$only!</h3>
                 <button>Take my money</button>
             </div>
 
 
         </main>
         <aside>
-            <img id=User_img src="<?= $faker->imageUrl($width, $height, 'cats', true, 'Faker')?>">
+            <img id=User_img src="{{data.user_img}}">
 
-            <h3><?= $faker->userName?></h3>
-            <h3><?= $faker->jobTitle?></h3>
+            <h3>{{ data.user_name}}</h3>
+            <h3>{{data.user_job}}</h3>
             <h3>Contact info</h3>
 
-            <p><?= $faker->email ?></p>
-            <p><?= $faker->phoneNumber ?></p>
+            <p> {{data.user_mail}}</p>
+            <p>{{data.user_phone}}</p>
 
-            <p> <?= $faker->streetAddress?></p>
-            <p> <?= $faker->postcode ." " . $faker->city  ?></p>
+            <p> {{data.user_adress}}</p>
+            <p> {{data.user_postcode}}  {{data.user_city}} </p>
 
 
         </aside>
 
 {% endblock %}
+  
